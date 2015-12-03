b810a120052196e531e04f690f4e59d63dd4d7b4
Chris White <chris.white@xdesign.com>
2015-12-03 09:32:50 +0000

    Improve day2 part 1 solution

--- day2/day2.php
+++ day2/day2.php
@@ -4,17 +4,11 @@ $presents = explode("\n", file_get_contents('input.txt'));
 $total = 0;
 
 foreach ($presents as $present) {
-    // 1x2x3 - length, width, height
-    list($length, $width, $height) = explode('x', $present);
+    list($l, $w, $h) = explode('x', $present);
 
-    $first = $length * $width;
-    $second = $width * $height;
-    $third = $height * $length;
-
-    $smallest = min($first, $second, $third);
-
-    $total += (2*$first) + (2*$second) + (2*$third);
-    $total += $smallest;
+    $total += (2 * $l * $w) + (2 * $w * $h) + (2 * $h * $l);
+    $total += min($l * $w, $w * $h, $h * $l);
 }
 
 echo "Total square feet of wrapping paper needed: $total\n";
+
